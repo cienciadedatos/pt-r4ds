dc95c4cd8b03c6c29ebe0a3ddac56d7814438563
Beatriz Milz <42153618+beatrizmilz@users.noreply.github.com>
2025-03-05 19:26:47 -0300

    imagens stat_count() #76

--- images/visualization/code-diamantes.R
+++ images/visualization/code-diamantes.R
@@ -14,3 +14,12 @@ plot_diamantes <- dados::diamante |>
   theme_bw(base_size = 15)
 
 ggsave("images/visualization/diamante.png", plot_diamantes, width = 6, height = 5, units = "in")
+
+
+plot_diamantes2 <- dados::diamante |>
+  ggplot() +
+  geom_bar(aes(x = corte), show.legend = FALSE) +
+  scale_fill_hue() +
+  theme_bw(base_size = 15)
+
+ggsave("images/visualization/diamante_bw.png", plot_diamantes2, width = 6, height = 5, units = "in")
